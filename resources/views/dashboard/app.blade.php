949bdcd0b3cde8c19ccc478266a5680751df4e7f
Linus <skinzip@gmail.com>
2019-12-09 12:26:05 +0100

    nav + sass

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -86,8 +86,6 @@
     </div>
     <!--NAVIGATION END-->
         
-        
-
         <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.bundle.min.js"></script>
         <script src="js/jquery.slimscroll.min.js"></script>
