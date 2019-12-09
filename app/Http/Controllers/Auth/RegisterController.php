5e6d0773b9d888a5c12b795e462d95d44ca14b13
Linus <skinzip@gmail.com>
2019-12-09 13:42:20 +0100

    login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/system';
 
     /**
      * Create a new controller instance.
