4e3b8aa361abafd48c0612cb567de2c87d8bd6e4
Claus Paludan <claus.paludan@gmail.com>
2019-12-19 16:28:16 +0100

    Simplifies the login/auth part of the menu
    Why:
    
    * The current version didn't handle the different states well.
    
    This commit:
    
    * Removes superflous code
    * Adds proper login check verification

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -8,15 +8,10 @@
             <li class="nav-item {{ (Route::getFacadeRoot()->current()->uri() == 'cal') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('/cal') }}">Calendar</a>
             </li>
-            <li class="nav-item {{ (Route::getFacadeRoot()->current()->uri() == '/system') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ url('/system') }}">Dashboard</a>
-            </li>
-            @auth
-                @can('accessAdminpanel')
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/system') }}">Admin</a>
+            @if(Auth::check())
+                <li class="nav-item {{ (Route::getFacadeRoot()->current()->uri() == '/system') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ url('/system') }}">Dashboard</a>
                 </li>
-                @endcan
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -30,7 +25,7 @@
                 <li class="nav-item {{ (Route::getFacadeRoot()->current()->uri() == '/register') ? 'active' : '' }}">
                     <a class="nav-link" href="{{ url('/register') }}">Register</a>
                 </li>
-            @endauth
+            @endif
         </ul>
     </div>
 </nav>
