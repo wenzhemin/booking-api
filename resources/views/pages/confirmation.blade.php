079941a4184cf6507e5eae590e07a5570a7dc244
Linus <skinzip@gmail.com>
2020-01-07 18:27:17 +0100

    a mix of a lot of fixes (im sorry)

--- resources/views/pages/confirmation.blade.php
+++ resources/views/pages/confirmation.blade.php
@@ -4,19 +4,20 @@
 
 
 <!--FRONTPAGE BANNER-->
-<div class="row banner bg-warning m-0">
-
-<div class="bg-warning">
-    <h1>Cinfirmed</h1> 
-    <p>Your booking have been made successfully.</p> 
-</div>
-
-
-    
-</div>
-
-<div>
-    <p><a href="/">Go to Home</a></p>
+<div id="confirmation">
+    <div class="text-center">
+        <img src="{{ asset('../assets/img/circle-logo.png') }}" width="200" height="200" alt="Booky circle logo">
+        <h1 class="display-3">Thank You!</h1>
+        <h2 class="display-3">Your booking has been successful, hope you have fun.</h2>
+        <p class="lead"><strong>Please check your email</strong> to review your booking information.</p>
+        <hr>
+        <p>
+        Having trouble? You are always welcome to contact us.
+        </p>
+        <p class="lead">
+        <a class="btn btn-back btn-sm" href="{{ url('/') }}" role="button">Continue to homepage</a>
+        </p>
+    </div>
 </div>
   
 
