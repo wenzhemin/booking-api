56a889e436f98628221062403f6766107bc19415
Claus Paludan <claus.paludan@gmail.com>
2020-01-08 17:12:13 +0100

    Add relations to the Booking and related models
    Why:
    
    * It was not possible to easily view relation fields when you were working with a booking
    
    This commit:
    
    * Adding relations to the Booking models and it's related models.
    
    This way it is possible to get the data from ie location when working with a booking. It is also possible to get a list of all bookings that uses a specific location or interval

--- app/Booking.php
+++ app/Booking.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    public function interval()
+    {
+        return $this->belongsTo('App\Interval');
+    }
+
+    public function location()
+    {
+        return $this->belongsTo('App\Location');
+    }
+
+    public function service()
+    {
+        return $this->belongsTo('App\Service');
+    }
 }
