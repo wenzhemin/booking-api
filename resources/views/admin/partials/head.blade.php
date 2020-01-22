3b196573eb6896cecd2b43872ecdc6b0d90da112
Linus <skinzip@gmail.com>
2020-01-22 23:34:19 +0100

    Added favicon
    WHY:
    * Because it was missing
    
    This commit:
    * Added the link that supplies the favicon

--- resources/views/admin/partials/head.blade.php
+++ resources/views/admin/partials/head.blade.php
@@ -1,7 +1,7 @@
 {{-- head --}}
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="icon" type="image/png" href="{{ asset('img/circle-logo.png') }}" sizes="16X16">
+<link rel="icon" type="image/png" sizes="16x16" href="assets/img/circle-logo.png">
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css">
